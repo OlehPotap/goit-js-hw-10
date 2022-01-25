4f4581960a5ec226bbeb3eadca60aed8897d82b9
OlehPotap <potap.oleh.v@gmail.com>
2022-01-25 13:03:37 +0200

    task10refactor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 'use ctrict';
 import './css/styles.css';
 import { fetchCountry } from './js/fetchCountries.js';
+import CountriesThatMatch from './handlebars/listItems.hbs';
+import CountryInfo from './handlebars/countryInfo.hbs';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
 const serchBoxEl = document.querySelector('#search-box');
-
-function callSerchCountries(event) {
-  document.querySelector('.country-info').innerHTML = '';
-  document.querySelector('.country-list').innerHTML = '';
-  fetchCountry(event.target.value.trim());
-}
+const listOfCountriesEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
 
 serchBoxEl.addEventListener('input', debounce(callSerchCountries, DEBOUNCE_DELAY));
+
+function callSerchCountries(event) {
+  const keyword = event.target.value.trim();
+  countryInfoEl.innerHTML = '';
+  listOfCountriesEl.innerHTML = '';
+  fetchCountry(keyword)
+    .then(response => {
+      if (response.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        return;
+      } else if (response.length > 2 && response.length <= 10) {
+        response.forEach(el => {
+          listOfCountriesEl.insertAdjacentHTML('beforeend', CountriesThatMatch(el));
+        });
+      } else {
+        response.map(el => {
+          countryInfoEl.insertAdjacentHTML('beforeend', CountryInfo(el));
+        });
+      }
+    })
+    .catch(err => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,36 +1,12 @@
-import Notiflix from 'notiflix';
-import CountriesThatMatch from '../handlebars/listItems.hbs';
-import CountryInfo from '../handlebars/countryInfo.hbs';
-
 const listOfCountriesEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
 
 export function fetchCountry(name) {
-  fetch(`https://restcountries.com/v3.1/name/${name}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(response => {
-      if (response.length > 10) {
-        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-        return;
-      } else if (response.length > 2 && response.length <= 10) {
-        response.forEach(el => {
-          listOfCountriesEl.insertAdjacentHTML('beforeend', CountriesThatMatch(el));
-          // document.querySelector('.country').addEventListener('click', () => {
-          //   document.querySelector('#search-box').value = el.name.common;
-          // });
-        });
-      } else {
-        response.map(el => {
-          countryInfoEl.insertAdjacentHTML('beforeend', CountryInfo(el));
-        });
-      }
-    })
-    .catch(err => {
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-    });
+  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    return response.json();
+  });
 }
